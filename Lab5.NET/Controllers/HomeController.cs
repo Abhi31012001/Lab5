4440e65f3d83bdad34dd0a429432491328ef72be
Abhi Patel <pate0789@algonquinlive.com>
2021-03-20 02:22:32 -0400

    First Commit

---
+++ Lab5.NET/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab5.NET.Models;
+namespace Lab5.NET.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        private readonly ILogger<HomeController> _logger;
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab5.NET/Data/AnswerImageDataContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab5.NET.Models;
+using Lab5.NET.Controllers;
+
+namespace Lab5.NET.Data
+{
+    public class AnswerImageDataContext : DbContext
+    {
+        public DbSet<AnswerImage> AnswerImage { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AnswerImage>().ToTable("AnswerImage");
+        }
+        public AnswerImageDataContext(DbContextOptions<AnswerImageDataContext> options)
+          : base(options)
+        {
+
+        }
+
+       
+
+    }
+}
